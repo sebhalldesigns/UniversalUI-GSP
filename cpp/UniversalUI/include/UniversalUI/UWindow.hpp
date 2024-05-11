f5317d4b88b89d73a7d213cbfae1199c4fa9f8c6
sebhall <sebhalldesigns@gmail.com>
2024-05-11 19:24:52 +0100

    mouse move box around test

--- cpp/UniversalUI/include/UniversalUI/UWindow.hpp
+++ cpp/UniversalUI/include/UniversalUI/UWindow.hpp
@@ -43,21 +43,36 @@ public:
         // MSVC
         #pragma warning(push)
         #pragma warning(disable: warning-code)
+
+            GWindowDrawDelegate drawDelegate = reinterpret_cast<GWindowDrawDelegate>(&Subclass::Draw);
+            GWindow_SetDrawDelegate(GWindowHandle, drawDelegate);
         
-            GWindowResizeDelegate resizeDelegate = reinterpret_cast<GWindowResizeDelegate>(&Subclass::WindowResized);
+            GWindowResizeDelegate resizeDelegate = reinterpret_cast<GWindowResizeDelegate>(&Subclass::Resized);
             GWindow_SetResizeDelegate(GWindowHandle, resizeDelegate);
+            
+            GWindowPointerMoveDelegate pointerMoveDelegate = reinterpret_cast<GWindowPointerMoveDelegate>(&Subclass::PointerMoved);
+            GWindow_SetPointerMoveDelegate(GWindowHandle, pointerMoveDelegate);
+            
 
         // GCC
         #pragma GCC diagnostic pop
         // MSVC
         #pragma warning(pop)
 
-
-
     }
 
-    virtual void WindowResized(GWindow window, GWindowSize size) {
+    virtual void Draw(GWindow window, GFrame surface) {
+        printf("BASE CLASS WINDOW DRAW\n");
+    }
+
+    virtual void Resized(GWindow window, GWindowSize size) {
         printf("BASE CLASS WINDOW RESIZED\n");
+    }
+
+
+
+    virtual void PointerMoved(GWindow window, GWindowPoint newLocation) {
+        printf("POINTER MOVED\n");
     }
 
 
--- cpp/UniversalUI/test/main.cpp
+++ cpp/UniversalUI/test/main.cpp
@@ -4,12 +4,61 @@
 #include "UniversalUI/UApplication.hpp"
 #include "UniversalUI/UWindow.hpp"
 
+GFrame myFrame = nullptr;
+int mouseX = 0;
+int mouseY = 0;
+
 class MyWindow: public UWindow<MyWindow> {
     public:
 
-    virtual void WindowResized(GWindow window, GWindowSize size) override {
+    
+    
+
+    virtual void Resized(GWindow window, GWindowSize size) override {
         printf("LAUNCH FROM MYWINDOW C++ CLASS %d %d\n", size.width, size.height);
 
+        
+
+    }
+
+    virtual void Draw(GWindow window, GFrame surface) override {
+        //printf("MY CLASS WINDOW DRAW\n");
+
+        
+        if (myFrame == nullptr) {
+            //GFrame_Free(myFrame);
+
+            GFrameInfo frameInfo = { 
+                10,
+                10
+            };
+    
+            myFrame = GFrame_Alloc(frameInfo);
+
+            printf("MYFRAME %lu\n", myFrame);
+
+
+            GColor color = { 0.0, 0.0, 1.0, 1.0};
+            GRect rect = { 0.0, 0.0, 10.0, 10.0};
+            GFrame_Fill(myFrame, rect, color);
+
+        }
+        
+        
+        GRect drawRect = { (float)mouseX, (float)mouseY, 10.0, 10.0};
+        GFrame_Composite(surface, drawRect, myFrame);
+    
+
+    }
+
+    virtual void PointerMoved(GWindow window, GWindowPoint newLocation) override {
+        //printf("POINTER MOVED\n");
+        mouseX = newLocation.x;
+        mouseY = newLocation.y;
+
+        //printf("%d %d\n", newLocation.x, newLocation.y);
+        
+        GWindow_Redraw(window);
     }
 };
 
